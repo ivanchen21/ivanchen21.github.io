f3ca66bac0e0feadf1e6efbbe0e3cfe2b3bb798d
Ivan <ivan@Ivans-MacBook-Air.local>
2024-04-08 00:02:50 -0400

    added basic website layout for museum of space science website

---
+++ src/assets/components/nav.js
@@ -0,0 +1,87 @@
+class header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <nav class="navbar navbar-expand-xl navbar-dark bg-black">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="homepage.html">
+                    <img src="/assets/images/logo11.jpg" alt="space science museum logo" class="d-inline-block align-text-top rounded-pill">
+                    <center><strong>GitScience</strong></center>
+                </a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="homepage.html">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="news.html">
+                                News
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="gallery.html">
+                                Gallery
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="about.html">
+                                About Us
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+      `;
+    }
+}
+
+//Must contain hyphen!
+customElements.define("navbar-header", header);
+
+
+class footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <footer class="nav-footer bg-black d-flex flex-wrap justify-content-evenly align-items-center py-3 my-4 border-top">
+            <div class="col-md-4 d-flex align-items-center">
+                <a href="homepage.html">
+                    <img src="/assets/images/logo11.jpg" alt="museum of space science logo">
+                    Ivan Chen
+                </a>
+            </div>
+
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <a href="#">
+                    <img src="/assets/images/GoTopIcon.png" alt="museum of space science logo">
+                </a>
+            </ul>
+        </footer>
+      `;
+    }
+}
+
+//Must contain hyphen!
+customElements.define("navbar-footer", footer);
