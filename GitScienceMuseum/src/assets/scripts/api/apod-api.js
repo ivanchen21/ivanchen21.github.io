e264de2aed4e197ed94ccb23ab47e7edb7091143
Ivan <ivan@Ivans-MacBook-Air.local>
2024-04-08 23:00:51 -0400

    added config file for api key, github pages static frontend

--- GitScienceMuseum/src/assets/scripts/api/apod-api.js
+++ GitScienceMuseum/src/assets/scripts/api/apod-api.js
@@ -3,7 +3,7 @@ import { NASA_API_KEY } from "../config.js";
 // Get today's APOD JSON from NASA API
 export async function fetchAPOD() {
     const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`;
-    // URL for testing video APOD
+    // URL for APOD videos (https://www.youtube.com/@APODVideos/videos)
     //const url = `https://api.nasa.gov/planetary/apod?api_key=h5gRoyD6NHDWX71M9tar8tQ0Mkma7cTVLuXf3NeX&date=2021-04-01`;
     const options = {
         method: 'GET',
---
+++ GitScienceMuseum/src/assets/scripts/config.js
@@ -0,0 +1,10 @@
+/*
+"If you are deploying a frontend-only application (github pages)
+you cannot hide anything from your deployed site. If your code needs 
+to access a value to make an API request that value will be visible 
+in the browser's dev tools to any user who feels like checking. 
+Any API request you make will be visible in the Network tab and will 
+show the full URL and any headers, which will expose the key."
+*/
+// /GitScienceMuseum/src/assets/scripts/config.js
+export const NASA_API_KEY = 'h5gRoyD6NHDWX71M9tar8tQ0Mkma7cTVLuXf3NeX';
